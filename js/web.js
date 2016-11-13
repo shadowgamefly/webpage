e1fa83e39339b63485f425bd2c9c999dcbbaad70
shadowgamefly <jerrysun1007@gmail.com>
2016-11-13 14:59:31 -0500

    carousel css

--- js/web.js
+++ js/web.js
@@ -6,7 +6,7 @@
         $("nav.navbar-wrapper.navbar-fixed-top").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
       } else {
         $("nav.navbar-wrapper.navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
-        $(".container-2 input#search").css("background-color", "transparent");
+        $(".container-2 input#search").css("background-color", "transparent"); 
       }
     });
   });
