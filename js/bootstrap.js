e9e3d32a9b86feb459ef3dbfc4b9753260a2e6f8
shadowgamefly <jerrysun1007@gmail.com>
2016-11-12 23:04:00 -0500

    navbar color change on scroll

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2374,16 +2374,5 @@ if (typeof jQuery === 'undefined') {
     })
   })
 
-// customize function
-  $(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 300) { // check if user scrolled more than 50 from top of the browser window
-        $("nav.navbar-wrapper.navbar-fixed-top").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-      } else {
-        $("nav.navbar-wrapper.navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
-        $(".container-2 input#search").css("background-color", "transparent");
-      }
-    });
-  });
 
 }(jQuery);
