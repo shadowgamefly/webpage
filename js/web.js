c290e975c6dcc1ea8a5f7a517b5ea2996ef27e6f
shadowgamefly <jerrysun1007@gmail.com>
2016-11-15 12:13:13 -0500

    fix bug in color and transition on search bar and navbar

--- js/web.js
+++ js/web.js
@@ -4,9 +4,10 @@
     $(window).scroll(function() { // check if scroll event happened
       if ($(document).scrollTop() > 300) { // check if user scrolled more than 50 from top of the browser window
         $("nav.navbar-wrapper.navbar-fixed-top").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        $(".container-2 input#search").css("background-color", "#333");
       } else {
         $("nav.navbar-wrapper.navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
-        $(".container-2 input#search").css("background-color", "transparent"); 
+        $(".container-2 input#search").css("background-color", "transparent");
       }
     });
   });
